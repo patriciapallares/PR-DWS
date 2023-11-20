20c0629be77e8fa366982e623a4f038e6480e270
patriciapallares <patriciapallaresgonzalez@gmail.com>
2023-11-20 13:17:06 +0100

    csv consultas hechas

--- funkos/src/main/java/org/example/Main.java
+++ funkos/src/main/java/org/example/Main.java
@@ -3,11 +3,13 @@ package org.example;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.sql.SQLOutput;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Stream;
 
 public class Main {
@@ -41,18 +43,104 @@ public class Main {
 
            // funkosFunkos.forEach(System.out::println);
 
-            // Funko más caro
+            // Funko más caro: PRECIO
             double precioMax = funkosFunkos.stream()
                     .map(Funko::getPrecio)
                     .max(Double::compareTo)
                     .orElse(null);
-
+            // Funko más caro: NOMBRE
+            System.out.println("FUNKO MÁS CARO:");
            funkosFunkos.stream()
                     .filter(funko -> funko.getPrecio() == precioMax)
                     .map(Funko::getNombre)
                     .forEach(System.out::println);
 
-            System.out.println(precioMax);
+            System.out.println("LA MEDIA ES:");
+            // media de precios de funkos
+            double mediaPrecioFunkos = funkosFunkos.stream()
+                    .mapToDouble(Funko::getPrecio)
+                    .average().getAsDouble();
+            System.out.println(mediaPrecioFunkos);
+/*
+
+            // TODO -  CONVERTIR EN FUNCIÓN CON PARÁMETROS
+            // funkos agrupados por modelos
+            System.out.println("FUNKOS MARVEL:");
+            funkosFunkos.stream()
+                    .filter(funko -> Objects.equals(funko.getModelo(), "MARVEL"))
+                    .map(Funko::getNombre)
+                    .forEach(System.out::println);
+
+            System.out.println("FUNKOS DISNEY:");
+            funkosFunkos.stream()
+                    .filter(funko -> Objects.equals(funko.getModelo(), "DISNEY"))
+                    .map(Funko::getNombre)
+                    .forEach(System.out::println);
+
+            System.out.println("FUNKOS ANIME:");
+            funkosFunkos.stream()
+                    .filter(funko -> Objects.equals(funko.getModelo(), "ANIME"))
+                    .map(Funko::getNombre)
+                    .forEach(System.out::println);
+
+            System.out.println("FUNKOS OTROS:");
+            funkosFunkos.stream()
+                    .filter(funko -> Objects.equals(funko.getModelo(), "OTROS"))
+                    .map(Funko::getNombre)
+                    .forEach(System.out::println);
+
+            */
+
+            // TODO -  CONVERTIR EN FUNCIÓN CON PARÁMETROS
+
+            System.out.println("NÚMERO DE FUNKOS MARVEL:");
+            long cantidadMarvel = funkosFunkos.stream()
+                    .filter(funko -> Objects.equals(funko.getModelo(), "MARVEL"))
+                    .count();
+
+            System.out.println(cantidadMarvel);
+
+            System.out.println("NÚMERO DE FUNKOS DISNEY:");
+            long cantidaDisney = funkosFunkos.stream()
+                    .filter(funko -> Objects.equals(funko.getModelo(), "DISNEY"))
+                    .count();
+
+            System.out.println(cantidaDisney);
+
+
+            System.out.println("NÚMERO DE FUNKOS ANIME:");
+            long cantidadAnime = funkosFunkos.stream()
+                    .filter(funko -> Objects.equals(funko.getModelo(), "ANIME"))
+                    .count();
+
+            System.out.println(cantidadAnime);
+
+
+            System.out.println("NÚMERO DE FUNKOS OTROS:");
+            long cantidadOtros = funkosFunkos.stream()
+                    .filter(funko -> Objects.equals(funko.getModelo(), "OTROS"))
+                    .count();
+
+            System.out.println(cantidadOtros);
+
+            /*
+            // date.getYear() >= yearStart && date.getYear() <= yearEnd
+
+            LocalDate dateStart =  LocalDate.of(23, 1, 1);
+            LocalDate dateEnd =  LocalDate.of(23, 12, 31);
+
+            funkosFunkos.stream().filter(funko -> funko.getFecha_lanzamiento() >= dateStart && funko.getFecha_lanzamiento()<= dateStart);
+
+            */
+
+            // funkos del 2023
+            System.out.println("fUNKO DEL 2023");
+            funkosFunkos.stream()
+                    .filter(funko -> funko.getFecha_lanzamiento().getYear() == 2023)
+                    .map(Funko::getNombre)
+                    .forEach(System.out::println);
+
+
 
         } catch (IOException e) {
             e.printStackTrace(System.out);
