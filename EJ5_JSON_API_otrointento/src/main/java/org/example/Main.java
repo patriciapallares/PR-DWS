7e82f3758e3b4b498e342f1f342188b0e0659877
patriciapallares <patriciapallaresgonzalez@gmail.com>
2023-12-11 13:55:13 +0100

    creado ej6

--- EJ5_JSON_API_otrointento/src/main/java/org/example/Main.java
+++ EJ5_JSON_API_otrointento/src/main/java/org/example/Main.java
@@ -12,7 +12,7 @@ import java.net.URL;
 import java.nio.file.Path;
 import java.util.List;
 
-public class MainPatri {
+public class Main {
     static List<Receta> recetasList;
     //añadimos el new JavaTimeModule, ya que es un módulo de Jackson que nos permite serializar y deserializar tipos de datos de fechas como por ejemplo: LocalDate, LocalTime, ZoneDateTime, etc
     //para usralo, hay que meter una dependencia Maven "com.fasterxml.jackson.datatype"
@@ -21,7 +21,7 @@ public class MainPatri {
     public static void main(String[] args) throws IOException {
         //el método .readTree nos devuelve un objeto de tipo JsonNode.
         //recetas con la "a"
-        JsonNode root = objectMapper.readTree(new URL("https://www.themealdb.com/api/json/v1/1/search.php?f=a"));
+        JsonNode root = objectMapper.readTree(new URL("https://www.themealdb.com/api/json/v1/1/search.php?f=b"));
 
         //debemos indicar la rama en la que deseamos buscar
         //el método .traverse() visita cada nodo hijo y realiza operaciones personalizadas en cada uno
