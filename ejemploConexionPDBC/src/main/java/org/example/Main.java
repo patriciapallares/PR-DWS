640cf286e9b2dbed2fd2f57209eec507dafd32de
patriciapallares <patriciapallaresgonzalez@gmail.com>
2023-12-18 14:04:48 +0100

    ahora funciona con pgAdmin

--- ejemploConexionPDBC/src/main/java/org/example/Main.java
+++ ejemploConexionPDBC/src/main/java/org/example/Main.java
@@ -6,9 +6,12 @@ public class Main {
     public static void main(String[] args) {
 
 
-        String sentenciaSQL = "SELECT * FROM personas";
+        String sentenciaSQL = "SELECT * FROM actores";
+        String usuario = "postgres";
+        String contrasenya = "postgres";
+
         // están entre parentesis dentro del try para que cierre la ejecución automáticamente...
-        try(Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/mi-base");
+        try(Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/mi-base", usuario, contrasenya);
             PreparedStatement sentencia = con.prepareStatement(sentenciaSQL)){
 
             ResultSet resultados = sentencia.executeQuery();
@@ -16,7 +19,10 @@ public class Main {
             while (resultados.next()){
                 String nombre = resultados.getString("nombre");
                 Integer edad = resultados.getInt("edad");
+                Integer id = resultados.getInt("id");
 
+                System.out.println("nombre: "+ nombre);
+                System.out.println(edad);
             }
 
         }catch (SQLException e){
