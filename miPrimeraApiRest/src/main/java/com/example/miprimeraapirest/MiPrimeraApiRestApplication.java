fb55af3f5742754d28c7a5b4a3d3642aebdd4eec
«Patricia» <patricia.clase@gmail.com>
2024-02-05 13:15:49 +0100

    prueba de commit

---
+++ miPrimeraApiRest/src/main/java/com/example/miprimeraapirest/MiPrimeraApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.miprimeraapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiPrimeraApiRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiPrimeraApiRestApplication.class, args);
+    }
+
+}
---
+++ miPrimeraApiRest/src/test/java/com/example/miprimeraapirest/MiPrimeraApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miprimeraapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiPrimeraApiRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
