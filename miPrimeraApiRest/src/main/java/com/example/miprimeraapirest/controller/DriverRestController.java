ffd6b7d25b68c76011ed7526a20291f20fdba595
«Patricia» <patricia.clase@gmail.com>
2024-02-07 12:33:53 +0100

    11 TERMINADO

--- miPrimeraApiRest/src/main/java/com/example/miprimeraapirest/controller/DriverRestController.java
+++ miPrimeraApiRest/src/main/java/com/example/miprimeraapirest/controller/DriverRestController.java
@@ -1,6 +1,5 @@
 package com.example.miprimeraapirest.controller;
 
-
 import com.example.miprimeraapirest.model.Driver;
 import com.example.miprimeraapirest.service.DriverService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+/*
+ *   http://localhost:8080/api/
+ * */
 @RestController
 @RequestMapping("/api")
 public class DriverRestController {
@@ -22,6 +24,9 @@ public class DriverRestController {
         this.driverService = driverService;
     }
 
+    /*
+     *   http://localhost:8080/api/drivers
+     * */
     @GetMapping("/drivers")
     public ResponseEntity<List<Driver>> getAll(){
         return ResponseEntity.ok(driverService.getAllDrivers());
